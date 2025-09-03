c810d1594e1d1fc6d9bc594d431c975594709629
Dudekula Sabitha <147849744+SabithaDudekula@users.noreply.github.com>
2025-09-03 22:49:24 +0530

    Delete car.js

--- car.js
+++
@@ -1,5 +0,0 @@
-function calculate() {
-  let members = document.getElementById("members").value;
-  let totalCars = Math.ceil(members / 5); // round up for leftover members
-  document.getElementById("result").innerText = "Cars required: " + totalCars;
-}

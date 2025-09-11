6f0ae204cecbd40a7da9c85a8eb626e578efc84a
Dudekula Sabitha <147849744+SabithaDudekula@users.noreply.github.com>
2025-09-11 14:06:30 +0530

    Add files via upload

---
+++ timer/timer.js
@@ -0,0 +1,74 @@
+let time = document.getElementById("time")
+let isTimeRunning = false
+
+let sec = 0;
+let min = 0;
+let hrs = 0;
+let clock;
+
+function startBtn() {
+    if (isTimeRunning) {
+        return
+    }
+    isTimeRunning = true
+    clock = setInterval(function () {
+        sec++;
+
+        if (sec == 60) {
+            min++;
+            sec = 0;
+        }
+        if (min == 60) {
+            hrs++;
+            min = 0;
+        }
+
+        let hours;
+        let minutes;
+        let seconds;
+
+        if (hrs < 10) {
+            hours = "0" + hrs
+        } else {
+            hours = hrs
+        }
+
+        if (min < 10) {
+            minutes = "0" + min
+        } else {
+            minutes = min
+        }
+
+        if (sec < 10) {
+            seconds = "0" + sec
+        } else {
+            seconds = sec
+        }
+
+        time.textContent = (hours + ":" + minutes + ":" + seconds)
+
+    }, 10)
+}
+
+function stopBtn() {
+    clearInterval(clock)
+    isTimeRunning = false
+}
+
+function resetBtn() {
+    hrs = 0;
+    min = 0;
+    sec = 0;
+    time.textContent = "00:00:00";
+    clearInterval(clock)
+    isTimeRunning = false;
+}
+
+
+
+
+
+
+
+
+
